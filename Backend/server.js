b6a3778038512b23edee3c357ac149d3c0ad305c
Akhilesh26 <akhileshpimple3@gmail.com>
2025-07-12 12:19:25 +0530

    create log in and register api

--- Backend/server.js
+++ Backend/server.js
@@ -30,8 +30,47 @@ app.use(cookieParser());
 ConnectDB.connectToPostgresSQL();
 ConnectDB.connectToMongo();
 
+const {createUser, 
+    logUser} = require("./Controllers/userController");
+
+
 app.get("/", (req, res)=>{
     res.json({message:"Hello from ReWear server you've reahed the start api"})
 })
+
+app.post('/api/login', async(req, res) => {
+    try {
+        const {username, password} = req.body;
+        console.log(username, password);
+        if(!username) return res.status(422).json({message:"Username is missing"});
+        if(!password) return res.status(422).json({message:"password is missing"});
+        
+        const DB_res = await logUser(username, password);
+        console.log(DB_res.data[0].userid)
+        console.log(DB_res.data[0].username)
+        
+        if(DB_res.status == 200){
+            const token = jwt.sign({
+                userId: DB_res.data[0].userid,
+                username: DB_res.data[0].username
+            }, process.env.JWT_SECRET);
+            res.cookie('userCookie', token);
+            res.status(200).json({message:"registration successful"});
+        }
+        else if(DB_res.status == 404){
+            res.status(DB_res.status).json({message:DB_res.message});
+        }
+        else{
+            res.status(DB_res.status).json({message:DB_res.message});
+        }
+
+    } catch (error) {
+        console.log("Error while loggin user = ",error);
+        res.status(500).json({message:"Server Error"})
+    }
+})
+
+
+
 
 app.listen(PORT, ()=>console.log("Server started at ", PORT));
