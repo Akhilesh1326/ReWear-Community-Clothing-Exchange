73db95ae797f3cf1e683987819aa52b64d1536c4
Akhilesh26 <akhileshpimple3@gmail.com>
2025-07-12 11:38:08 +0530

    basic server run

---
+++ Backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const PORT = process.env.PORT || 8000;
+const ConnectDB = require("./ConnectDB");
+const cors = require('cors');
+const bodyParser = require('body-parser')
+require('dotenv').config();
+
+// JWT related imports
+const cookieParser = require('cookie-parser');
+const http = require('http');
+const jwt = require('jsonwebtoken');
+
+// Server by express
+const app = express()
+
+// cors policy
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE'],
+    credentials: true
+}));
+ 
+// Middlewares
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+// Connection of Databases as Postgre and MongoDB
+ConnectDB.connectToPostgresSQL();
+ConnectDB.connectToMongo();
+
+app.get("/", (req, res)=>{
+    res.json({message:"Hello from ReWear server you've reahed the start api"})
+})
+
