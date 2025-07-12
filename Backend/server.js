5b571e0e071eee57c362438fb21d852a814328fc
Akhilesh26 <akhileshpimple3@gmail.com>
2025-07-12 13:02:15 +0530

    update for categories and listings

--- Backend/server.js
+++ Backend/server.js
@@ -70,7 +70,83 @@ app.post('/api/login', async(req, res) => {
     }
 })
 
+// Listing realted api calls
+app.get("/api/categories", async(req, res)=>{
+    try {
+        const db_resp = await getAllCategories();
+        if(db_resp.status == 200) return res.status(db_resp.status).json({message: db_resp.message, data: db_resp.data});
+        
+        res.status(db_resp.status).json({message: db_resp.message});
+    } catch (error) {
+        console.log("error in server for getting all categories ", error);
+        res.status(500).json({message:"Internal Server Error"});
+    }
+});
 
+app.get("/api/category/:id", async(req,res)=>{
+    try {
+        const id = parseInt(req.params.id);
+
+        if(isNaN(id)){
+            return res.status(400).json({message:"Invalid category Id"});
+        }
+
+        const DB_res = await getCategoryById(id);
+
+        if(DB_res.status == 200) return res.status(DB_res.status).json({message: DB_res.message, data: DB_res.data});
+        res.status(DB_res.status).json({message: DB_res.message});
+
+    } catch (error) {
+        console.log("Error in server for getting category by id");
+        res.status(500).json({message:"Internal server error"});
+    }
+})
+
+app.get("/api/listing/:itemId", async (req, res) => {
+    try {
+        const itemId = req.params.itemId;
+
+        if (!itemId || typeof itemId !== "string") {
+            return res.status(400).json({ message: "Invalid item ID" });
+        }
+
+        const DB_res = await getListingById(itemId);
+
+        if (DB_res.status === 200)
+            return res.status(DB_res.status).json({ message: DB_res.message, data: DB_res.data });
+
+        res.status(DB_res.status).json({ message: DB_res.message });
+
+    } catch (error) {
+        console.error("Error in server for getting listing by itemId:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+app.get("/api/listings/:categoryId", async (req, res) => {
+    try {
+        const categoryId = req.params.categoryId;
+
+        if (!categoryId || typeof categoryId !== "string") {
+            return res.status(400).json({ message: "Invalid category ID" });
+        }
+
+        const DB_res = await getAllListings(categoryId);
+
+        if (DB_res.status === 200) {
+            return res.status(DB_res.status).json({
+                message: DB_res.message,
+                data: DB_res.data
+            });
+        }
+
+        return res.status(DB_res.status).json({ message: DB_res.message });
+
+    } catch (error) {
+        console.error("Error in server for getting listings by categoryId:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
 
 
 app.listen(PORT, ()=>console.log("Server started at ", PORT));
