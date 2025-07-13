179ede42f5c973f06c3891bf785737156048411a
Akhilesh26 <akhileshpimple3@gmail.com>
2025-07-13 18:35:02 +0530

    new routes add

--- Frontend/router.jsx
+++ Frontend/router.jsx
@@ -6,8 +6,9 @@ import ReWearAuth from "./src/auth/ReWearAuth"
 import DashBoard from "./src/pages/DashBoard"
 import Item_listing from "./src/pages/Item_listing"
 import Item_page from "./src/pages/Item_page"
-import LandingPage from "./src/pages/LandingPage"
+import LandingPage from "./src/pages/LandingPage";
 import ListItemPage from "./src/pages/ListItemPage";
+import AdminPanel from "./src/pages/AdminPanel";
 
 
 const router = createBrowserRouter([
@@ -18,6 +19,7 @@ const router = createBrowserRouter([
     {path: "/landingpage", element: <LandingPage/>},
     {path: "/itemlisting", element: <Item_listing/>},
     {path: "/list-item-page", element: <ListItemPage/>},
+    {path: "/admin", element: <AdminPanel/>},
 
 ]);
 
